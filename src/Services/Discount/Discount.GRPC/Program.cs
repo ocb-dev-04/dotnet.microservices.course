5d44189deb94ad6ead44e3bf41aae96b431a1f82
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-04-28 15:12:04 -0400

    *Remove comments from Program.cs

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -19,8 +19,6 @@ namespace Discount.GRPC
             host.Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
