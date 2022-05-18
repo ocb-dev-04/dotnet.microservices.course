e3663587703b5cbff42c10e3448df99af62d0f01
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-05-18 01:00:24 -0400

    *add order services with dll and all

---
+++ src/Services/Order/Order.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Order.Domain.Entities;
+
+namespace Order.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<OrderModel>
+    {
+        Task<IEnumerable<OrderModel>> GetOrderByUserName(string userName);
+    }
+}
